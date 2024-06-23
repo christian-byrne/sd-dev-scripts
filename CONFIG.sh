ec58cb88beb4fadb72c8e2e8edcfdc301487df6d
christian-byrne <abolkonsky.rem@gmail.com>
2024-06-23 12:32:12 -0700

    stableswarm venv start script

--- CONFIG.sh
+++ CONFIG.sh
@@ -4,14 +4,7 @@
 #                                  USER CONFIG                                 #
 # ---------------------------------------------------------------------------- #
 
-# ----------------------------------- Paths ---------------------------------- #
 USER_PATH="/home/c_byrne"
-
-TEST_ENV="$USER_PATH/projects/comfy-testing-environment"
-COMFY_DIRNAME="ComfyUI"
-CUSTOM_NODES_DIRNAME="custom_nodes"
-
-STABLE_DIFFUSION_WEBUI_DIR="$USER_PATH/tools/sd/sd-interfaces/stable-diffusion-webui"
 
 # ---------------------------- Virtual Environment --------------------------- #
 VIRTUAL_ENV_DIRNAME="venv"
@@ -23,13 +16,27 @@ CHROME_PROFILE_NAME="Default"
 DEVTOOLS_WINDOW_DEFAULT="console"
 
 # ---------------------- Stable Diffusion WebUI Options ---------------------- #
+STABLE_DIFFUSION_WEBUI_DIR="$USER_PATH/tools/sd/sd-interfaces/stable-diffusion-webui"
 SD_WEBUI_STARTUP_DELAY="5.12"
 SD_WEBUI_CLI_ARGS="--xformers --no-half-vae"
+SD_WEBUI_INJECT_FILENAME="launch.py"
 
 # ------------------------------- Comfy Options ------------------------------ #
 COMFY_PORT="8188"
 COMFY_STARTUP_DELAY="2.56"
 COMFY_STARTUP_CLI_ARGS=""
+TEST_ENV="$USER_PATH/projects/comfy-testing-environment"
+COMFY_DIRNAME="ComfyUI"
+CUSTOM_NODES_DIRNAME="custom_nodes"
+COMFU_INJECT_FILENAME="main.py"
+
+# ---------------------------- StableSwarm Options --------------------------- #
+STABLESWARM_PORT="7801"
+STABLESWARM_STARTUP_DELAY="2.56"
+STABLESWARM_STARTUP_CLI_ARGS=""
+STABLESWARM_DIRNAME="StableSwarm"
+STABLESWARM_TEST_ENV="$USER_PATH/projects/stableswarm-testing-environment"
+STABLESWARM_INJECT_FILENAME="launch-linux.sh"
 
 # ---------------------------------- Logging --------------------------------- #
 MAX_LOG_LINES=3000
@@ -47,7 +54,9 @@ SD15_DEFAULT_FILENAME="v1-5-pruned-emaonly.safetensors"
 # ---------------------------------------------------------------------------- #
 #                                  DONT CHANGE                                 #
 # ---------------------------------------------------------------------------- #
+SD_WEBUI_FULLPATH="$STABLE_DIFFUSION_WEBUI_DIR"
 COMFY_DIR_FULLPATH=$(find $TEST_ENV -type d -name $COMFY_DIRNAME)
+STABLESWARM_FULLPATH=$(find $STABLESWARM_TEST_ENV -type d -name $STABLESWARM_DIRNAME)
 
 CUSTOM_NODES_DIR_FULLPATH="$COMFY_DIR_FULLPATH/$CUSTOM_NODES_DIRNAME"
 DEACTIVATED_NODES_DIR_FULLPATH="$COMFY_DIR_FULLPATH/$DEACTIVATED_NODES_DIRNAME"
---
+++ start-stableswarm.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# ---------------------------------------------------------------------------- #
+#                                   Constants                                  #
+# ---------------------------------------------------------------------------- #
+
+THIS_SCRIPT_PATH=$(realpath $0)
+THIS_SCRIPT_PARENT_DIR=$(dirname $THIS_SCRIPT_PATH)
+. $THIS_SCRIPT_PARENT_DIR/CONFIG.sh
+
+# ---------------------------------------------------------------------------- #
+#                               Option Arguments                               #
+# ---------------------------------------------------------------------------- #
+
+USE_VIRTUALENV=false
+
+# Parse arguments
+while [[ "$#" -gt 0 ]]; do
+		case $1 in
+				-h|--help)
+						echo "Usage: start-stableswarm.sh"
+						echo "Options:"
+						echo "-h, --help        Show this help message and exit"
+						echo "-v, --use-virtualenv        Use virtual environment"
+						exit 0
+						;;
+				-v|--use-virtualenv)
+						USE_VIRTUALENV=true
+						;;
+				*)
+						echo "Unknown parameter passed: $1"
+						exit 1
+						;;
+		esac
+		shift
+done
+
+
+# ---------------------------------------------------------------------------- #
+
+cd $STABLESWARM_FULLPATH
+
+if [ "$USE_VIRTUALENV" = true ]; then
+	source $VIRTUAL_ENV_DIRNAME/bin/activate
+	export LD_LIBRARY_PATH=$VIRTUAL_ENV/lib64/python$VENV_PYTHON_VERSION/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
+else
+	export LD_LIBRARY_PATH=/usr/lib64/python$VENV_PYTHON_VERSION/site-packages/nvidia/nvjitlink/lib:$LD_LIBRARY_PATH
+fi
+
+bash ./$STABLESWARM_INJECT_FILENAME $STABLESWARM_STARTUP_CLI_ARGS
